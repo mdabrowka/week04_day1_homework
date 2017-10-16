c154f156121c8d52096a547cedfa7eb004a64a91
Marta <martadabrowka@gmail.com>
2017-10-16 21:50:22 +0100

    added erb file

--- controller.rb
+++ controller.rb
@@ -6,5 +6,6 @@ require_relative('./models/paper_rock_scissors.rb')
 
 get '/play_game/:hand_1/:hand_2' do
   game = PaperRockScissors.new(params[:hand_1], params[:hand_2])
-  return game.play_game
+  @game_result = game.play_game
+  erb(:result)
 end
