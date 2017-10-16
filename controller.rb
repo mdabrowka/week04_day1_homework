2c201f3a3e5c030a89b2a9b8e0563d8c729162bd
Marta <martadabrowka@gmail.com>
2017-10-16 22:04:56 +0100

    welcome added

--- controller.rb
+++ controller.rb
@@ -3,6 +3,9 @@ require('sinatra/contrib/all')
 require_relative('./models/paper_rock_scissors.rb')
 
 
+get '/welcome' do
+  erb(:welcome)
+end
 
 get '/play_game/:hand_1/:hand_2' do
   game = PaperRockScissors.new(params[:hand_1], params[:hand_2])
