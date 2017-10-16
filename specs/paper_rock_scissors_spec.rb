0b8df9187c42be7d3d96610442a8685cbe7abc57
Marta <martadabrowka@gmail.com>
2017-10-16 21:03:55 +0100

    got case statement to work

--- specs/paper_rock_scissors_spec.rb
+++ specs/paper_rock_scissors_spec.rb
@@ -3,10 +3,22 @@ require_relative '../models/paper_rock_scissors.rb'
 
 class TestPaperRockScissors < Minitest::Test
 
-def setup
-@game = PaperRockScissors.new("rock", "scissors")
-end
+  def setup
+    @game = PaperRockScissors.new("rock", "scissors")
+    @game1 = PaperRockScissors.new("rock", "paper")
+    @game2 = PaperRockScissors.new("paper", "paper")
+  end
 
+   def test_rock_scissors
+     assert_equal("Hand_1 wins", @game.play_game)
+   end
 
+    def test_rock_paper
+      assert_equal("Hand_2 wins", @game1.play_game)
+    end
+
+    def test_rock_rock
+      assert_equal("Draw", @game2.play_game)
+    end
 
 end
