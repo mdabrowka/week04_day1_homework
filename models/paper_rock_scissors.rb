f94f72b4f5a5e956919b74fe856c0218477e774a
Marta <martadabrowka@gmail.com>
2017-10-16 22:15:01 +0100

    final touches

--- models/paper_rock_scissors.rb
+++ models/paper_rock_scissors.rb
@@ -8,14 +8,14 @@ class PaperRockScissors
   def play_game
 
     case
-    when (@hand_1 == "rock" && @hand_2 == "paper"); return "Hand_2 wins with paper"
-    when (@hand_1 == "rock" && @hand_2 == "scissors"); return "Hand_1 wins with rock"
+    when (@hand_1 == "rock" && @hand_2 == "paper"); return "Player 2 wins with paper"
+    when (@hand_1 == "rock" && @hand_2 == "scissors"); return "Player 1 wins with rock"
     # when (@hand_1 == "rock" && @hand_2 == "rock"); return "Draw"
-  when (@hand_1 == "paper" && @hand_2 == "rock"); return "Hand_1 wins with paper"
-  when (@hand_1 == "paper" && @hand_2 == "scissors"); return "Hand_2 wins with scissors"
+  when (@hand_1 == "paper" && @hand_2 == "rock"); return "Player 1 wins with paper"
+  when (@hand_1 == "paper" && @hand_2 == "scissors"); return "Player 2 wins with scissors"
     # when (@hand_1 == "paper" && @hand_2 == "paper"); return "Draw"
-  when (@hand_1 == "scissors" && @hand_2 == "rock"); return "Hand_2 wins with rock"
-  when (@hand_1 == "scissors" && @hand_2 == "paper"); return "Hand_1 wins with scissors"
+  when (@hand_1 == "scissors" && @hand_2 == "rock"); return "Player 2 wins with rock"
+  when (@hand_1 == "scissors" && @hand_2 == "paper"); return "Player 1 wins with scissors"
     # when (@hand_1 == "scissors" && @hand_2 == "scissors"); return "Draw"
     when @hand_1 = @hand_2; return "Draw"
       end
